7b9ad2342e955aae16cf7524c7ac6712b28cf38f
Kapavarapu <durgadevi.kapavarapu@accenture.com>
2025-07-07 14:18:18 +0530

    committing terrform

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group" "sg-terraform"{
+    name = "sg-terraform"
+    description = "all inbound traffic and all outbound traffic"
+}
+
+
+# resource "aws_instance" "test-server"{
+    
+# }
